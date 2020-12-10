6f557bf747e9831ec449a9f60e3c4fe6443bcfe5
Jose Toledo <toledojose60@gmail.com>
2020-12-10 11:56:34 -0800

    added habitat dictionaries

--- blm_data_cleaning.py
+++ blm_data_cleaning.py
@@ -10,60 +10,23 @@ sccwrp_xls = pd.ExcelFile("C:/Users/toled/Desktop/SCCWRP/SCCWRP_SWAMP_FieldDataS
 sccwrp_field_results = sccwrp_xls.parse('sccwrp_swamp_fielddatasheet_0')
 
 
-Field_Analytes = {'WaterTemperature': 'Temperature',
-                  'WaterTempDuplicate' : 'Temperature',
-                  'AirTemperature': 'Temperature',
-                  'WaterpH': 'pH','WaterpHDuplicate': 'pH',
-                  'AirWindSpeed': 'WindSpeed',
-                  'WaterDOSaturation': 'Oxygen, Saturation',
-                  'WaterDOSatDuplicate': 'Oxygen, Saturation',
-                  'WaterDOConcentration': 'Oxygen, Dissolved',
-                  'WaterDODuplicate': 'Oxygen, Dissolved',
-                  'WaterSalinity': 'Salinity',
-                  'WaterSalinityDuplicate': 'Salinity',
-                  'WetStreamWidth': 'WettedWidth',
-                  'StationWaterDepth': 'StationWaterDepth',
-                  'FlowMeterSect1Depth': 'StationWaterDepth',
-                  'FlowMeterSect2Depth': 'StationWaterDepth',
-                  'FlowMeterSect3Depth': 'StationWaterDepth',
-                  'FlowMeterSect4Depth': 'StationWaterDepth',
-                  'FlowMeterSect5Depth': 'StationWaterDepth',
-                  'FlowMeterSect1Velocity': 'Velocity',
-                  'FlowMeterSect2Velocity': 'Velocity',
-                  'FlowMeterSect3Velocity': 'Velocity',
-                  'FlowMeterSect4Velocity': 'Velocity',
-                  'FlowMeterSect5Velocity': 'Velocity'}
+Field_matrix_dict = relationships_map.loc[0:25,['OriginalAnalyteName','MatrixName']].set_index('OriginalAnalyteName').to_dict()
+Field_analytes_dict = relationships_map.loc[0:25,['OriginalAnalyteName','AnalyteName']].set_index('OriginalAnalyteName').to_dict()
 
 
-Habitat_Analytes = {'SiteOdor': 'Odor',
-                  'WaterOdor': 'Odor',
-                  'WaterColor': 'Color',
-                  'WaterClarity': 'WaterClarity',
-                  'SkyCode': 'SkyCode',
-                  'WindDirection': 'WindDirection',
-                  'Precipitation': 'Precipitation',
-                  'PrecipitationLast24hrs': 'PrecipitationLast24hrs',
-                  'EvidenceofFires': 'Evidence of Fire',
-                  'OverlandRunoff': 'OverlandRunoff',
-                  'DominantMaterial': 'DominantSubstrate',
-                  'BeaufortScale': 'BeaufortScale',
-                  'ObservedFlow': 'ObservedFlow'}
+Field_Matrix_Name = Field_matrix_dict['MatrixName']
+Field_Analytes = Field_analytes_dict['AnalyteName']
 
 
-
-dict1 = relationships_map.loc[0:25,['OriginalAnalyteName','MatrixName']].set_index('OriginalAnalyteName').to_dict()
-dict2 = relationships_map.loc[0:25,['OriginalAnalyteName','AnalyteName']].set_index('OriginalAnalyteName').to_dict()
-Field_Matrix_Name = dict1['MatrixName']
-Field_Analytes = dict2['AnalyteName']
+Field_Analytes.keys()
 
 
+Habitat_matrix_dict = relationships_map.loc[26:40,['OriginalAnalyteName','MatrixName']].set_index('OriginalAnalyteName').to_dict()
+Habitat_analytes_dict = relationships_map.loc[26:40,['OriginalAnalyteName','AnalyteName']].set_index('OriginalAnalyteName').to_dict()
 
 
-
-
-
-
-
+Habitat_Matrix_Name = Habitat_matrix_dict['MatrixName']
+Habitat_Analytes = Habitat_analytes_dict['AnalyteName']
 
 # clean up blm_field_results data
 
