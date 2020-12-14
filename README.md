cfa06046cd14c058dd837a2bf004b924200d362f
Robert Butler <robertb@sccwrp.org>
2020-12-14 13:27:07 -0800

    not sure what has changed.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # BLM_data_reformatter
-Script{s) and other things related to grabbing raw BLM field data and reformatting it to SWAMP's liking
+Script(s) and other things related to grabbing raw BLM field data and reformatting it to SWAMP's liking
--- blm_data_cleaning.py
+++ blm_data_cleaning.py
@@ -1,61 +1,63 @@
 import pandas as pd
 from pandasgui import show
 
+# Read in the relationship map
+relationships_analytes = pd.ExcelFile("input/RelationshipMap.xlsx").parse('Analytes')
+relationships_columns = pd.ExcelFile("input/RelationshipMap.xlsx").parse('Columns')
 
-relationships_analytes = pd.ExcelFile("RelationshipMap.xlsx").parse('Analytes')
-relationships_columns = pd.ExcelFile("RelationshipMap.xlsx").parse('Columns')
 # this is the original dataset
+sccwrp_field_results = pd.read_excel("input/SCCWRP_SWAMP_FieldDataSheet.xlsx", sheet_name = 'sccwrp_swamp_fielddatasheet_0')
 
-
-sccwrp_xls = pd.ExcelFile("SCCWRP_SWAMP_FieldDataSheet.xlsx")
-sccwrp_field_results = sccwrp_xls.parse('sccwrp_swamp_fielddatasheet_0')
-
+# tack on missing columns
+sccwrp_field_results.assign(BatchVerificationCode = '', QACode = '', ResQualCode = '=')
+    
+ 
 # create field analytes
 field_filter = relationships_analytes["AnalyteNameType"] == "Field"
-Field_matrix_dict = relationships_analytes.loc[field_filter, ['OriginalAnalyteName', 'MatrixName']].set_index('OriginalAnalyteName').to_dict()
-Field_analytes_dict = relationships_analytes.loc[field_filter, ['OriginalAnalyteName', 'AnalyteName']].set_index('OriginalAnalyteName').to_dict()
-Field_unitname_dict = relationships_analytes.loc[field_filter, ['OriginalAnalyteName', 'UnitName']].set_index('OriginalAnalyteName').to_dict()
 
-
-Field_Matrix_Name = Field_matrix_dict['MatrixName']
-Field_Analytes = Field_analytes_dict['AnalyteName']
-Field_UnitName = Field_unitname_dict['UnitName']
-
+field = {
+    **relationships_analytes.loc[field_filter, ['OriginalAnalyteName', 'MatrixName']].set_index('OriginalAnalyteName').to_dict(),
+    **relationships_analytes.loc[field_filter, ['OriginalAnalyteName', 'AnalyteName']].set_index('OriginalAnalyteName').to_dict(),
+    **relationships_analytes.loc[field_filter, ['OriginalAnalyteName', 'UnitName']].set_index('OriginalAnalyteName').to_dict()
+}
 
 # create habitat analytes
 habitat_filter = relationships_analytes["AnalyteNameType"] == "Habitat"
-Habitat_matrix_dict = relationships_analytes.loc[habitat_filter, ['OriginalAnalyteName', 'MatrixName']].set_index('OriginalAnalyteName').to_dict()
-Habitat_analytes_dict = relationships_analytes.loc[habitat_filter, ['OriginalAnalyteName', 'AnalyteName']].set_index('OriginalAnalyteName').to_dict()
-Habitat_unitname_dict = relationships_analytes.loc[habitat_filter, ['OriginalAnalyteName', 'UnitName']].set_index('OriginalAnalyteName').to_dict()
 
-Habitat_Matrix_Name = Habitat_matrix_dict['MatrixName']
-Habitat_Analytes = Habitat_analytes_dict['AnalyteName']
-Habitat_UnitName = Habitat_unitname_dict['UnitName']
+habitat = {
+    **relationships_analytes.loc[habitat_filter, ['OriginalAnalyteName', 'MatrixName']].set_index('OriginalAnalyteName').to_dict(),
+    **relationships_analytes.loc[habitat_filter, ['OriginalAnalyteName', 'AnalyteName']].set_index('OriginalAnalyteName').to_dict(),
+    **relationships_analytes.loc[habitat_filter, ['OriginalAnalyteName', 'UnitName']].set_index('OriginalAnalyteName').to_dict()
+}
 
 # create melted field data
-
 field_tabs = ['All', 'FieldResults']
 
 field_IDvars = relationships_columns.loc[relationships_columns['Tab'].isin(field_tabs), 'OriginalColumn']
-field_melt_dat = pd.melt(sccwrp_field_results, id_vars=field_IDvars, value_vars=list(Field_Analytes))
+field_melt_dat = pd.melt(sccwrp_field_results, id_vars=field_IDvars, value_vars=list(field['AnalyteName']))
 
 
 field_melt_dat['MatrixName'] = field_melt_dat['variable']
-field_melt_dat['MatrixName'].replace(Field_Matrix_Name, inplace=True)
+field_melt_dat['MatrixName'].replace(field['MatrixName'], inplace=True)
 
 
 field_melt_dat['UnitName'] = field_melt_dat['variable']
-field_melt_dat['UnitName'].replace(Field_UnitName, inplace=True)
+field_melt_dat['UnitName'].replace(field['UnitName'], inplace=True)
 
 
-field_melt_dat["variable"].replace(Field_Analytes, inplace=True)
-field_melt_dat.rename(columns= {'variable': 'Analyte',
-                       'value': 'Result'}, inplace=True)
+field_melt_dat["variable"].replace(field['AnalyteName'], inplace=True)
+field_melt_dat.rename(
+    columns= {
+        'variable': 'Analyte',
+        'value': 'Result'
+    }, 
+    inplace=True
+)
 
 field_melt_dat['SampleDuplicatesTaken'].fillna(1,inplace=True)
 field_melt_dat['SampleDuplicatesTaken'].replace({'Yes': int(2), 'No': int(1)}, inplace=True)
 
-field_duplicates_filter = ((field_melt_dat['SampleDuplicatesTaken'] ==1) & (field_melt_dat['Result'].isna()))
+field_duplicates_filter = ((field_melt_dat['SampleDuplicatesTaken'] == 1) & (field_melt_dat['Result'].isna()))
 field_results_dat = field_melt_dat[-field_duplicates_filter]
 
 field_results_dat['Result'].fillna(-88,inplace=True)
@@ -66,23 +68,28 @@ field_results_dat['Result'].fillna(-88,inplace=True)
 habitat_tabs = ['All', 'HabitatResults']
 
 habitat_IDvars = relationships_columns.loc[relationships_columns['Tab'].isin(habitat_tabs), 'OriginalColumn']
-habitat_melt_dat = pd.melt(sccwrp_field_results, id_vars=habitat_IDvars, value_vars=list(Habitat_Analytes))
+habitat_melt_dat = pd.melt(sccwrp_field_results, id_vars=habitat_IDvars, value_vars=list(habitat['AnalyteName']))
 
-habitat_melt_dat["variable"].replace(Habitat_Analytes, inplace=True)
+habitat_melt_dat["variable"].replace(habitat['AnalyteName'], inplace=True)
 
 
 habitat_melt_dat['MatrixName'] = habitat_melt_dat['variable']
-habitat_melt_dat['MatrixName'].replace(Habitat_Matrix_Name, inplace=True)
+habitat_melt_dat['MatrixName'].replace(habitat['MatrixName'], inplace=True)
 
 
 habitat_melt_dat['UnitName'] = habitat_melt_dat['variable']
-habitat_melt_dat['UnitName'].replace(Habitat_UnitName, inplace=True)
+habitat_melt_dat['UnitName'].replace(habitat['UnitName'], inplace=True)
 
 
-habitat_melt_dat["variable"].replace(Habitat_Analytes, inplace=True)
+habitat_melt_dat["variable"].replace(habitat['AnalyteName'], inplace=True)
 
-habitat_melt_dat.rename(columns= {'variable': 'Analyte',
-                       'value': 'Result'}, inplace=True)
+habitat_melt_dat.rename(
+    columns= {
+        'variable': 'Analyte',
+        'value': 'Result'
+    }, 
+    inplace=True
+)
 
 habitat_melt_dat['HabitatReplicate'].fillna(1, inplace=True)
 habitat_melt_dat['HabitatReplicate'].replace({'No': int(1)}, inplace=True)
@@ -91,12 +98,10 @@ habitat_duplicates_filter = ((habitat_melt_dat['HabitatReplicate'] == 1) & (habi
 habitat_results_dat = habitat_melt_dat[-habitat_duplicates_filter]
 
 
-
-
 # create excel file with field/habitat results as sheets
-blm_swampformat = pd.ExcelWriter('blm_swampformat.xlsx', engine='xlsxwriter')
+blm_swampformat = pd.ExcelWriter('output/blm_swampformat.xlsx', engine='xlsxwriter')
 
-field_results_dat.to_excel(blm_swampformat, sheet_name='FieldResults')
-habitat_results_dat.to_excel(blm_swampformat, sheet_name='HabitatResults')
+field_results_dat.sort_values(['StationID','SampleDate']).to_excel(blm_swampformat, sheet_name='FieldResults')
+habitat_results_dat.sort_values(['StationID','SampleDate']).to_excel(blm_swampformat, sheet_name='HabitatResults')
 
 blm_swampformat.save()
